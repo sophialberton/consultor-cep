efe29a532e803c807b8084847a63968fa02f56f3
Sophia <sophia.alberton@gmail.com>
2025-03-07 17:30:19 -0300

    criação de pastar e configurações feitas para compilar código em C ANSI

---
+++ Desafio-1/Fibonacci.c
@@ -0,0 +1,20 @@
+/*1. Sequência de Fibonacci
+Crie um programa que, dado um número inteiro n, retorne os primeiros n números da
+sequência de Fibonacci.
+Exemplo de entrada:
+n = 6
+Exemplo de saída:
+[0, 1, 1, 2, 3, 5]*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void){
+
+
+    prinf("Teste! Olá mundo!");
+
+
+
+    return 0;
+}
